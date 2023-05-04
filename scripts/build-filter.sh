db87db1582efafa65e2f85fc0f479825ffc97939
Samee Ibraheem <sameeibraheem@Samees-Air.askey.com>
2023-05-04 16:16:09 -0700

    Filter questions

---
+++ scripts/build-filter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Safely execute this bash script
+# e exit on first failure
+# x all executed commands are printed to the terminal
+# u unset variables are errors
+# a export all variables to the environment
+# E any trap on ERR is inherited by shell functions
+# -o pipefail | produces a failure code if any stage fails
+set -Eeuoxa pipefail
+
+# Get the directory of this script
+LOCAL_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker build -t lamini-datasets:latest $LOCAL_DIRECTORY/.. --file $LOCAL_DIRECTORY/../Dockerfile-Filter
+
