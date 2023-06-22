15a7ec322ce46ab9e616a0eba9decb0179e988d4
Samee Ibraheem <sameeibraheem@Samees-Air.askey.com>
2023-06-22 15:53:18 -0700

    Update seed data loading

--- generate_data.py
+++ generate_data.py
@@ -1,15 +1,9 @@
-import llama
-
 from llama import Type, Context, LLM
-
 from llama.error.error import APIError as LlamaAPIError
 
-import time
 import jsonlines
 import random
 import argparse
-
-import os
 
 
 def main():
@@ -18,16 +12,25 @@ def main():
     )
 
     parser.add_argument(
-        "-c", "--count", default=100, help="The number of examples to generate."
+        "-c", "--count", default=100, help="The number of examples to generate; default is 100."
     )
     parser.add_argument(
         "-b",
         "--batch-size",
         default=10,
-        help="The number of examples to generate in a batch.",
+        help="The number of examples to generate in a batch; default is 10.",
     )
     parser.add_argument(
-        "-r", "--response_data", default=False, help="Whether to use seed data as examples for response generation.", type=bool
+        "-r", "--response_data", default=False, help="Whether to use seed data as examples for response generation; default is False.", type=bool
+    )
+    parser.add_argument(
+        "-s", "--seed_file", default="seed_tasks.jsonl", help="The path to the seed dataset file; default is 'seed_tasks.jsonl'."
+    )
+    parser.add_argument(
+        "-q", "--question_key", default="question", help="The key for each question in the seed dataset; default is 'question'."
+    )
+    parser.add_argument(
+        "-a", "--answer_key", default="answer", help="The key for each answer in the seed dataset; default is 'answer'."
     )
 
     arguments = vars(parser.parse_args())
@@ -35,13 +38,16 @@ def main():
     total_examples = int(arguments["count"])
     batch_size = int(arguments["batch_size"])
     add_response_data = bool(arguments["response_data"])
+    seed_file = arguments["seed_file"]
+    question_key = arguments["question_key"]
+    answer_key = arguments["answer_key"]
 
     for count in range(0, total_examples, batch_size):
         if count + batch_size > total_examples:
             batch_size = total_examples - count
         print(f"Processing index {count} out of {total_examples} using batch size {batch_size}")
-        generate_questions(start_index=count, batch_size=batch_size)
-        generate_responses(index=count, batch_size=batch_size, add_response_data=add_response_data)
+        generate_questions(index=count, batch_size=batch_size, path=seed_file, key=question_key)
+        generate_responses(index=count, batch_size=batch_size, path=seed_file, question_key=question_key, answer_key=answer_key, add_response_data=add_response_data)
 
 
 class Question(Type):
@@ -52,34 +58,34 @@ class NovelQuestion(Type):
     question: str = Context("a novel question, with a radically different subject")
 
 
-def generate_questions(start_index, batch_size):
+def generate_questions(index, batch_size, path, key):
 
     with open("data/questions.jsonl", "a") as questions_file:
         writer = jsonlines.Writer(questions_file, flush=True)
 
         llm = LLM(name="generate-lamini")
 
-        seed_instructions = list(load_seed_dataset())
+        seed_questions = list(load_questions(path, key))
 
-        llm.add_data(make_pairs(seed_instructions))
+        llm.add_data(make_pairs(seed_questions))
 
-        for index in range(start_index, start_index + batch_size):
-            instruction = seed_instructions[index % len(seed_instructions)]
-            print("====== Seed Question =====\n", instruction)
-            novel_question = get_question(llm, instruction)
+        for index in range(index, index + batch_size):
+            question = seed_questions[index % len(seed_questions)]
+            print("====== Seed Question =====\n", question)
+            novel_question = get_question(llm, question)
 
             novel_question.question = parse(novel_question.question)
             print("===== Novel Question =====\n", novel_question)
             writer.write(novel_question.dict())
 
-def get_question(llm, instruction):
+def get_question(llm, question):
 
     attempts = 5
 
-    for i in range(attempts):
+    for _ in range(attempts):
         try:
             return llm(
-                input=instruction,
+                input=question,
                 output_type=NovelQuestion,
                 temperature=0.7,
                 model_name="lamini/open",
@@ -92,12 +98,12 @@ def get_question(llm, instruction):
 
 
 
-def make_pairs(seed_instructions):
+def make_pairs(seed_questions):
     pairs = []
-    for seed in seed_instructions:
-        other = random.sample(seed_instructions, 1)[0]
+    for question in seed_questions:
+        other = random.sample(seed_questions, 1)[0]
 
-        pairs.append([seed, NovelQuestion(question=other.question)])
+        pairs.append([question, NovelQuestion(question=other.question)])
 
     return pairs
 
@@ -118,15 +124,11 @@ def parse(string):
     return string
 
 
-def load_seed_dataset():
-    return load_questions(path="seed_tasks.jsonl", key="instruction")
-
-
-def load_questions(path, key="question"):
+def load_questions(path, key='question'):
     with open(path) as questions_file:
         reader = jsonlines.Reader(questions_file)
 
-        for index, line in enumerate(reader):
+        for _, line in enumerate(reader):
             yield Question(
                 question=line[key],
             )
@@ -141,10 +143,7 @@ class QuestionAndResponse(Type):
     response: str = Context("the response to the question")
 
 
-def load_seed_instances():
-    return load_questions_and_answers("seed_tasks.jsonl")
-
-def load_questions_and_answers(path, question_key="instruction", answer_key="output"):
+def load_questions_and_answers(path, question_key, answer_key):
     questions_and_answers = []
     with open(path) as questions_file:
         reader = jsonlines.Reader(questions_file)
@@ -157,7 +156,7 @@ def load_questions_and_answers(path, question_key="instruction", answer_key="out
             )])
     return questions_and_answers
 
-def generate_responses(index, batch_size, add_response_data=False):
+def generate_responses(index, batch_size, path, question_key, answer_key, add_response_data):
     questions = list(load_questions(path="data/questions.jsonl"))
 
     with open("data/dataset.jsonl", "a") as dataset_file:
@@ -166,7 +165,7 @@ def generate_responses(index, batch_size, add_response_data=False):
         llm = LLM(name="generate-lamini-reponse")
 
         if add_response_data:
-            llm.add_data(load_seed_instances())
+            llm.add_data(load_questions_and_answers(path, question_key, answer_key))
 
         for question in questions[index:index+batch_size]:
             print("====== Question =====\n", question)
@@ -183,7 +182,7 @@ def get_response(llm, question):
 
     attempts = 5
 
-    for i in range(attempts):
+    for _ in range(attempts):
         try:
             return llm(
                 input=question,
